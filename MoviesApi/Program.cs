169d8f083d18e30d23e19f6f575b86e149aa47d0
Lokesh.Gadde <lokesh.gadde@infosys.com>
2025-05-14 13:25:18 +0530

    programcs

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,12 +1,11 @@
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MovieApi.Middleware;
 using MovieApi.Models; // For MongoDbSettings
 using MovieApi.Service;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-
 // Ensure the "FileCache" folder is deleted on app initialization
 var cacheFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "FileCache");
 if (Directory.Exists(cacheFolderPath))
@@ -14,28 +13,34 @@ if (Directory.Exists(cacheFolderPath))
     Directory.Delete(cacheFolderPath, true);
 }
 
+
 var builder = WebApplication.CreateBuilder(args);
 
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+};
+var client = new SecretClient(new Uri(builder.Configuration["KeyVault:Uri"] ?? throw new InvalidOperationException("KeyVault Uri is not configured")), new DefaultAzureCredential(), options);
 
+KeyVaultSecret secret = client.GetSecret("MongoDB--ConnectionString");
 
-// Configure Azure Key Vault
-string keyVaultUri = builder.Configuration["KeyVault:Uri"] ?? 
-    throw new InvalidOperationException("Key Vault URI is not configured.");
-var client = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
-var secretName = "MongoDB--ConnectionString";
-var secretValue = client.GetSecret(secretName).Value.Value;
-Console.WriteLine($"Your secret is '{secretValue}'.");
-
-var DatabaseName = client.GetSecret("MongoDB--DatabaseName").Value.Value;
-Console.WriteLine($"Your secret is '{DatabaseName}'.");
+string ConnectionString = secret.Value;
+KeyVaultSecret databaseName = client.GetSecret("MongoDB--DatabaseName");
+string DatabaseName = databaseName.Value;
 
 // Configure services
-builder.Services.Configure<MongoDbSettings>(options =>
-{
-    options.ConnectionString = secretValue;
-    options.DatabaseName = DatabaseName;
-});
-
+builder.Services.Configure<MongoDbSettings>(
+    options =>
+    {
+        options.ConnectionString = ConnectionString;
+        options.DatabaseName = DatabaseName;
+    });
 builder.Services.AddSingleton<MongoDbService>();
 builder.Services.AddSingleton<IMoviesService, MoviesService>();
 
@@ -48,10 +53,11 @@ builder.Services.AddCors(config =>
 {
     config.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "http://localhost:5075")
+        // Allow all origins, methods, and headers
+        // Note: In production, you should specify the allowed origins instead of using "*"
+        policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
+              .AllowAnyHeader();
     });
 });
 
