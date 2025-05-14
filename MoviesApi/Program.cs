9007d50c8647d32165cc728fa1f9dc2e69a05a4c
Lokesh.Gadde <lokesh.gadde@infosys.com>
2025-05-14 12:41:00 +0530

    revert program.cs

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using MovieApi.Middleware;
 using MovieApi.Models; // For MongoDbSettings
 using MovieApi.Service;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-
 // Ensure the "FileCache" folder is deleted on app initialization
 var cacheFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "FileCache");
 if (Directory.Exists(cacheFolderPath))
@@ -14,23 +10,13 @@ if (Directory.Exists(cacheFolderPath))
     Directory.Delete(cacheFolderPath, true);
 }
 
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Azure Key Vault
-string keyVaultUri = builder.Configuration["KeyVault:Uri"] ?? 
-    throw new InvalidOperationException("Key Vault URI is not configured.");
-
-builder.Configuration.AddAzureKeyVault(
-    new Uri(keyVaultUri),
-    new DefaultAzureCredential());
-
 // Configure services
-builder.Services.Configure<MongoDbSettings>(options =>
-{
-    options.ConnectionString = builder.Configuration["MongoDB:ConnectionString"];
-    options.DatabaseName = builder.Configuration["MongoDB:DatabaseName"];
-});
-
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
 builder.Services.AddSingleton<MongoDbService>();
 builder.Services.AddSingleton<IMoviesService, MoviesService>();
 
